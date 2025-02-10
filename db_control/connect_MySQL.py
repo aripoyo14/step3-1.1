76daef95d0073a743411516655d2e6e38a80ee14
aripoyo14 <s.14.arimura@gmail.com>
2025-02-10 10:37:51 +0900

    first commit

---
+++ db_control/connect_MySQL.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+
+import os
+from dotenv import load_dotenv
+
+# 環境変数の読み込み
+load_dotenv()
+
+# データベース接続情報
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = os.getenv('DB_PORT')
+DB_NAME = os.getenv('DB_NAME')
+
+# SSL証明書のパス
+ssl_cert = str('DigiCertGlobalRootCA.crt.pem')
+
+# MySQLのURL構築
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# エンジンの作成（SSL設定を追加）
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={
+        "ssl": {
+            "ssl_ca":ssl_cert
+        }
+    },
+    echo=True,
+    pool_pre_ping=True,
+    pool_recycle=3600
+)
+
